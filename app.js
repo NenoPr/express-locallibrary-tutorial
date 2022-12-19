882695f745aa5010174041d258d2be567a54a3a3
NenoPr <nenadpredan@gmail.com>
2022-12-19 19:53:59 +0100

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app = express();
 const mongoose = require("mongoose");
 // Set up mongoose connection
 const dev_db_url =
-  "mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
+  "mongodb+srv://admin:841995@cluster0.xk83bew.mongodb.net/?retryWrites=true&w=majority";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
