6b320143753d7526f98e13ef3387c0ff2c9b5b1c
NenoPr <nenadpredan@gmail.com>
2022-12-19 20:08:16 +0100

    Removed duplicate import.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const catalogRouter = require("./routes/catalog"); // Import routes for "catalog" area of site
 const compression = require("compression");
 const helmet = require("helmet");
 
